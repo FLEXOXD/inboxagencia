95932072f84e33faceec94558f5a425d9e5f88f4
FLEXOXD <davidflexjoker@gmail.com>
2023-09-02 12:34:14 -0500

    mejoras del footer

--- application/views/inicio/footer.php
+++ application/views/inicio/footer.php
@@ -1,75 +1,148 @@
-<footer class="site-footer">
-  <div class="site-footer-top">
-  <div class="container">
-      <div class="row">
-          <div class="col-lg-6 col-12 d-flex align-items-center">
-              <img src="<?php echo base_url('assets/imagen/iconoblanco.png'); ?>" alt="Logo" class="me-3" width="45">
-              <h2 class="regular">inbox</h2><h2 class="black">.agencia</h2>
 
-          </div>
-      </div>
-  </div>
-</div>
+<footer class="bg1">
+    <div class="site-footer-top">
     <div class="container">
         <div class="row">
-            <div class="col-lg-9 col-12 mb-4 pb-2">
-                <h5 class="extrabold mb-3">Enlaces</h5>
-                <ul class="site-footer-links">
-                    <li class="site-footer-link-item">
-                        <a href="<?php echo site_url(); ?>" class="medium">Inicio</a>
-                    </li>
-                    <li class="site-footer-link-item">
-                        <a href="<?php echo site_url('Agencias/index'); ?>" class="medium">Agencia</a>
-                    </li>
-                    <li class="site-footer-link-item">
-                        <a href="#" class="medium">Portafolio</a>
-                    </li>
-                    <li class="site-footer-link-item">
-                        <a href="#" class="medium">Clientes</a>
-                    </li>
-                    <li class="site-footer-link-item">
-                        <a href="#" class="medium">Contactanos</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
-                <h5 class="extrabold mb-3">Buscanos</h5>
-                <p class="medium d-flex mb-1">
-                    <a href="https://goo.gl/maps/yy7FebzpnhcdsnkF9" class="medium" target="_blank">
-                        Quito-Ecuador (Clic)
-                    </a>
-                </p>
-                <p class="medium d-flex mb-1">
-                    <a href="https://api.whatsapp.com/send?phone=5939905551281&text=Hola%20desde%20Quito!" class="medium" target="_blank">
-                        0995551281 (WhatsApp)
-                    </a>
-                </p>
-                <p class="medium d-flex">
-                    <a href="mailto:hello@company.com?subject=Consulta%20desde%20Quito" class="medium">
-                        hello@company.com (clic envianos un correo)
-                    </a>
-                </p>
-            </div>
-            <div class="col-lg-3 col-md-6 col-11 mb-4 mb-lg-0 mb-md-0">
-                <h5 class="extrabold mb-3">Servicios</h5>
-                <p class="medium d-flex mt-3 mb-2">
-                    Diseño Gráfico - Diseño Web</p>
+            <div class="col-lg-6 col-12 d-flex align-items-center">
+                <img src="<?php echo base_url('assets/imagen/iconoblanco.png'); ?>" alt="Logo" class="me-3" width="45">
+                <h2 class="regular">inbox</h2><h2 class="black">.agencia</h2>
+
             </div>
         </div>
     </div>
-    <div class="site-footer-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-9 col-12 mt-5">
-                    <p class="copyright-text">InboxAgencia 2023 Agencia </p>
-                </div>
-                <div class="col-lg-3 col-12 mt-5">
-                    <p class="copyright-text">Derechos Reservados</p>
-                </div>
-            </div>
-        </div>
-    </div></center>
-</footer>
+  </div>
+		<div class="container p-t-40 p-b-70">
+			<div class="row">
+				<div class="col-sm-6 col-md-4 p-t-50">
+					<!-- - -->
+					<h4 class="medium m-b-33">
+						Contactanos
+					</h4>
+
+          <ul class="m-b-70">
+              <li class="regular m-b-14">
+                  <i class="fa fa-map-marker fs-16 dis-inline-block size19" aria-hidden="true"></i>
+                  <a href="https://goo.gl/maps/BVQP5cRgqkeUmcEh8" class="regular" target="_blank">Quito-Ecuador</a>
+              </li>
+              <li class="regular m-b-14">
+                  <i class="fa fa-phone fs-16 dis-inline-block size19" aria-hidden="true"></i>
+                  <a href="https://api.whatsapp.com/send?phone=593997640853&text=Hola%20desde%20Quito!" class="regular" target="_blank">(+593) 997 640 853</a>
+              </li>
+
+              <li class="regular m-b-14">
+                  <i class="fa fa-envelope fs-13 dis-inline-block size19" aria-hidden="true"></i>
+                  <a href="mailto:gerencia@inboxagencia.com" class="regular">gerencia@inboxagencia.com</a>
+              </li>
+          </ul>
+					<!-- - -->
+					<h4 class="medium m-b-32">
+						Horarios de apertura
+					</h4>
+
+					<ul>
+						<li class="regular">
+							08:30 AM – 7:00 PM
+						</li>
+
+						<li class="regular">
+							Lunes - Viernes
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-sm-6 col-md-4 p-t-50">
+					<!-- - -->
+					<h4 class="medium m-b-33">
+						Latest twitter
+					</h4>
+
+					<div class="m-b-25">
+						<span class="fs-13 color2 m-r-5">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</span>
+						<a href="#" class="regular">
+							@colorlib
+						</a>
+
+						<p class="regular m-b-18">
+							Activello is a good option. It has a slider built into that displays the featured image in the slider.
+							<a href="#" class="regular">
+								https://buff.ly/2zaSfAQ
+							</a>
+						</p>
+
+						<span class="regular">
+							21 Dec 2017
+						</span>
+					</div>
+
+					<div>
+						<span class="fs-13 color2 m-r-5">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</span>
+						<a href="#" class="regular">
+							@colorlib
+						</a>
+
+						<p class="regular m-b-18">
+							Activello is a good option. It has a slider built into that displays
+							<a href="#" class="regular">
+								https://buff.ly/2zaSfAQ
+							</a>
+						</p>
+
+						<span class="regular">
+							21 Dec 2017
+						</span>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-4 p-t-50">
+					<!-- - -->
+					<h4 class="medium m-b-38">
+						Certificados
+					</h4>
+
+					<!-- Gallery footer -->
+					<div class="wrap-gallery-footer flex-w">
+						<a class="item-gallery-footer wrap-pic-w" href="<?php echo base_url('assets/imagen/certificados/1.png'); ?>" data-lightbox="gallery-footer">
+							<img src="<?php echo base_url('assets/imagen/certificados/1.png'); ?>" alt="GALLERY">
+						</a>
+
+						<a class="item-gallery-footer wrap-pic-w" href="<?php echo base_url('assets/imagen/certificados/2.png'); ?>" data-lightbox="gallery-footer">
+							<img src="<?php echo base_url('assets/imagen/certificados/2.png'); ?>" alt="GALLERY">
+						</a>
+
+						<a class="item-gallery-footer wrap-pic-w" href="<?php echo base_url('assets/imagen/certificados/1.png'); ?>" data-lightbox="gallery-footer">
+							<img src="<?php echo base_url('assets/imagen/certificados/1.png'); ?>" alt="GALLERY">
+						</a>
+
+						<a class="item-gallery-footer wrap-pic-w" href="<?php echo base_url('assets/imagen/certificados/2.png'); ?>" data-lightbox="gallery-footer">
+							<img src="<?php echo base_url('assets/imagen/certificados/2.png'); ?>" alt="GALLERY">
+						</a>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="end-footer bg2">
+			<div class="container">
+				<div class="flex-sb-m flex-w p-t-22 p-b-22">
+					<div class="p-t-5 p-b-5">
+						<a href="https://www.instagram.com/inbox.agencia/" class="fs-15 c-white"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+						<a href="#" class="fs-15 c-white"><i class="fa fa-behance m-l-18" aria-hidden="true"></i></a>
+						<a href="#" class="fs-15 c-white"><i class="fa fa-linkedin m-l-18" aria-hidden="true"></i></a>
+            <a href="https://www.tiktok.com/@inboxagencia" class="fs-15 c-white"><i class="bi-tiktok m-l-18" aria-hidden="true"></i></a>
+					</div>
+
+					<div class="bold p-r-20 p-t-5 p-b-5">
+						Copyright © 2023 | Creado por Inbox.Agencia<i class="fa fa-heart"></i> by <a href="https://colorlib.com" target="_blank">inbox.Agencia</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 <!-- Back to top -->
 <div class="btn-back-to-top bg0-hov" id="myBtn">
   <span class="image-btn-back-to-top">
